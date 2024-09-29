96bc61b6fe42a1061151f4fd65a83d1214bdecb6
Chrispian <chrispian@gmail.com>
2024-09-29 14:17:46 -0500

    Fixed missing sort_order field on User model

--- app/Models/PostCategory.php
+++ app/Models/PostCategory.php
@@ -2,14 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\EloquentSortable\Sortable;
 use Spatie\EloquentSortable\SortableTrait;
 
 class PostCategory extends Model implements Sortable
 {
-    use HasFactory;
     use SortableTrait;
 
     protected $guarded = [];
