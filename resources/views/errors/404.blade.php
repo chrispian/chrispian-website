f60a077dac323425cd673347fdd364e0e20bb6b8
Chrispian <chrispian@gmail.com>
2024-09-29 19:46:54 -0500

    Mobile tweaks for 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -14,7 +14,7 @@
         <!-- This is an example component -->
         <div class="error_404 w-2/3 mx-auto flex items-center content-center">
             <div class="w-full flex flex-col md:flex-row items-center justify-center px-5 text-gray-700">
-                <div class="max-w-md">
+                <div class="max-w-md ">
                     <div class="text-5xl text-[#8cfbe6] font-bold">404</div>
                     <p class="text-2xl md:text-3xl font-light text-orange-700">
                         Sorry we couldn't find this page.
@@ -28,8 +28,7 @@
                     </button>
 
                 </div>
-                <div class="max-w-lg">
-
+                <div class="mt-8">
                     <img class="shadow-[0px_0px_15px_10px_#8cfbe6] rounded-full items-center content-center place-content-center justify-center center justify-self-center w-[150px] mx-auto" src="http://chrispian.com/storage/media/a7e4d93d-9dc5-4364-8d7e-7de5589d6c95.jpg" alt="Young kid climbing through a doggie door making a face to be funny." />
                 </div>
 
